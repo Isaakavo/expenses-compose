f14e8fec24f45f7efb851d9d9c3c46d8e432951c
isaakavo <isaakhaas96@gmail.com>
2023-12-10 12:55:42 -0600

    add catch to flow call

--- app/src/main/java/com/avocado/expensescompose/presentation/cards/cardswithexpenses/ExpensesTotalByCardViewModel.kt
+++ app/src/main/java/com/avocado/expensescompose/presentation/cards/cardswithexpenses/ExpensesTotalByCardViewModel.kt
@@ -16,6 +16,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.flow.zip
@@ -104,7 +105,9 @@ class ExpensesTotalByCardViewModel @Inject constructor(private val graphQlClient
             CardsWithExpensesState()
           }
         }
-      }.collect {
+      }
+        .catch { e -> MyResult.Error(uiText = "Error from network ${e.localizedMessage}", data = null) }
+        .collect {
         _state.emit(it)
       }
     }
