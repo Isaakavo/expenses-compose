adc9f559d52e1b922b1cac12d3376b0a70000b0b
isaakavo <isaakhaas96@gmail.com>
2023-12-20 12:53:29 -0600

    improve data validation fun

--- app/src/main/java/com/avocado/expensescompose/data/adapters/graphql/utils/GraphqlUtils.kt
+++ app/src/main/java/com/avocado/expensescompose/data/adapters/graphql/utils/GraphqlUtils.kt
@@ -1,5 +1,8 @@
 package com.avocado.expensescompose.data.adapters.graphql.utils
 
+import android.util.Log
+import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.api.Operation
 import com.apollographql.apollo3.exception.ApolloException
 import com.avocado.expensescompose.data.model.MyResult
 
@@ -12,3 +15,14 @@ fun <D> validateData(data: D): MyResult<D> = try {
 } catch (e: ApolloException) {
   MyResult.Error(data = null, uiText = "Something went wrong from the server")
 }
+
+fun <D : Operation.Data> validateDataWithoutErrors(
+  data: ApolloResponse<D>,
+  uiText: String = "Algo salio mal Dx"
+): MyResult<D> =
+  try {
+    MyResult.Success(data.dataAssertNoErrors)
+  } catch (e: ApolloException) {
+    Log.e("VALIDATE_DATA", e.localizedMessage.orEmpty())
+    MyResult.Error(data = data.data, uiText = uiText, exception = e)
+  }
