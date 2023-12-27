72671f61fc390304d337456c119166143d2e70ce
isaakavo <isaakhaas96@gmail.com>
2023-12-27 16:54:17 -0600

    delete unused import

--- app/src/main/java/com/avocado/expensescompose/presentation/incomes/incomewithexpense/IncomeExpensesScreen.kt
+++ app/src/main/java/com/avocado/expensescompose/presentation/incomes/incomewithexpense/IncomeExpensesScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
