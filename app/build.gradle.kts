98cf66cb25467e6430fb5b1c589836ee73f99be7
isaakavo <isaakhaas96@gmail.com>
2023-12-29 14:37:24 -0600

    add timber library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,9 @@ dependencies {
   //Navigation
   implementation("androidx.navigation:navigation-compose:2.7.5")
 
+  // Timber
+  implementation("com.jakewharton.timber:timber:5.0.1")
+
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/avocado/expensescompose/MainActivity.kt
+++ app/src/main/java/com/avocado/expensescompose/MainActivity.kt
@@ -6,15 +6,21 @@ import androidx.activity.compose.setContent
 import com.avocado.expensescompose.presentation.navigation.ExpensesApplication
 import com.avocado.expensescompose.ui.theme.ExpensesComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ExpensesComposeTheme {
-               ExpensesApplication()
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    if (BuildConfig.DEBUG) {
+      Timber.plant(Timber.DebugTree())
     }
+
+    setContent {
+      ExpensesComposeTheme {
+        ExpensesApplication()
+      }
+    }
+  }
 }
