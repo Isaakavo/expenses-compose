54a0ba20d9ebfd297c90df5c11cd0ee6f9bd185b
isaakavo <isaakhaas96@gmail.com>
2024-01-25 22:59:05 -0600

    bump packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,23 +88,23 @@ android {
 dependencies {
 
   implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-rc01")
-  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-rc01")
-  implementation("androidx.activity:activity-compose:1.8.1")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+  implementation("androidx.activity:activity-compose:1.8.2")
   implementation(platform("androidx.compose:compose-bom:2023.10.01"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3:1.1.2")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0-rc01")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
   //Grapqhl
   implementation("com.apollographql.apollo3:apollo-runtime:3.8.2")
 
   //Hilt
-  implementation("com.google.dagger:hilt-android:2.49")
+  implementation("com.google.dagger:hilt-android:2.50")
   implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-  kapt("com.google.dagger:hilt-android-compiler:2.44")
+  kapt("com.google.dagger:hilt-android-compiler:2.50")
   kapt("androidx.hilt:hilt-compiler:1.1.0")
 
   //Retrofit
@@ -117,7 +117,7 @@ dependencies {
   implementation("androidx.datastore:datastore-preferences:1.0.0")
 
   //Navigation
-  implementation("androidx.navigation:navigation-compose:2.7.5")
+  implementation("androidx.navigation:navigation-compose:2.7.6")
 
   // Timber
   implementation("com.jakewharton.timber:timber:5.0.1")
