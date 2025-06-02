b908eb993389ee21f9205d03a0931981cfd6942f
isaakavo <isaakhaas96@gmail.com>
2025-06-01 17:38:36 -0700

    refactor of ExpensesListEvents

--- app/src/main/java/com/avocado/expensescompose/presentation/expenses/allexpenses/viewmodel/AllExpensesListEvents.kt
+++ app/src/main/java/com/avocado/expensescompose/presentation/expenses/allexpenses/viewmodel/AllExpensesListEvents.kt
@@ -1,9 +1,9 @@
 package com.avocado.expensescompose.presentation.expenses.allexpenses.viewmodel
 
 sealed class AllExpensesListEvents {
-  object DeleteExpense : AllExpensesListEvents()
-  object ApplyFilter : AllExpensesListEvents()
-  object UpdateDeleteExpenseId : AllExpensesListEvents()
-  object UpdateSuccessDelete : AllExpensesListEvents()
-  object RestoreLists : AllExpensesListEvents()
+  data object DeleteExpense : AllExpensesListEvents()
+  data object ApplyFilter : AllExpensesListEvents()
+  data object UpdateDeleteExpenseId : AllExpensesListEvents()
+  data object UpdateSuccessDelete : AllExpensesListEvents()
+  data object RestoreLists : AllExpensesListEvents()
 }
