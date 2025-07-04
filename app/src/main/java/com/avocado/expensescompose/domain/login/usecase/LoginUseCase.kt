d2635c7a228cbb1124257b4c92aecdedae61358b
isaakavo <isaakhaas96@gmail.com>
2025-07-04 14:22:14 -0700

    refactor: remove unused saveUsername function from LoginUseCase

--- app/src/main/java/com/avocado/expensescompose/domain/login/usecase/LoginUseCase.kt
+++ app/src/main/java/com/avocado/expensescompose/domain/login/usecase/LoginUseCase.kt
@@ -55,7 +55,5 @@ class LoginUseCase @Inject constructor(
       }
   }
 
-  suspend fun saveUsername(username: String) = authRepository.saveUsername(username)
-
   suspend fun resetLoginCredentials() = authRepository.resetTokens()
 }
